232c163fd52d6a77e041ef988c058db9ed9909ea
curtis <curtis@serverascode.com>
2022-02-02 15:53:04 -0500

    add svc for weighted routes

--- workshop/setup.d/setup-ingress.sh
+++ workshop/setup.d/setup-ingress.sh
@@ -2,3 +2,9 @@
 
 envsubst < ~/demo/ingress.yaml.envsubst > ~/demo/ingress.yaml
 rm ~/demo/ingress.yaml.envsubst
+
+envsubst < ~/demo/http-echo-one.yaml.envsubst > ~/demo/http-echo-one.yaml
+rm ~/demo/http-echo-one.yaml.envsubst
+
+envsubst < ~/demo/http-echo-two.yaml.envsubst > ~/demo/http-echo-two.yaml
+rm ~/demo/http-echo-two.yaml.envsubst
